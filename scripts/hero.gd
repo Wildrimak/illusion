cb505dea420fa7dc35337208fe132cc95824b022
Wildrimak <finalwildrimak@gmail.com>
2019-09-29 21:53:56 -0300

    Se move, e aparece objetos

--- scripts/hero.gd
+++ scripts/hero.gd
@@ -1,5 +1,28 @@
-extends RigidBody2D
+extends KinematicBody2D
 
+var motion = Vector2()
+var speed = 300
+const UP = Vector2(0, -1)
+
+func _physics_process(delta):
+	
+	motion.y += 20
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = speed 
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -speed
+	elif Input.is_action_pressed("ui_down"):
+		pass
+	else:
+		motion.x = 0
+	
+	if is_on_floor():
+		if Input.is_action_pressed("ui_up"):
+			motion.y = -1200
+	
+	move_and_slide(motion, UP)	
+	
 func _ready():
 	set_process_input(true)
 	
@@ -9,7 +32,4 @@ func _input(event):
 		on_touch()
 
 func on_touch():
-	apply_impulse(Vector2(0, 0), Vector2(0, -1000))
-
-	
-	
+	$".".position.y-=20
