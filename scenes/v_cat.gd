b78ebfda812c8e234f6bcfe8e1c10b4ce8ccd953
Wildrimak <finalwildrimak@gmail.com>
2019-09-29 23:43:08 -0300

    pronto

--- scenes/v_cat.gd
+++ scenes/v_cat.gd
@@ -3,9 +3,22 @@ extends StaticBody2D
 var flip = true
 var init
 var end
+var velocidade = 3
 
 func _ready():
 	$AnimatedSprite.play("walk")
+	init = $".".position.x
+	end = init + 400
 
 func _process(delta):
-	pass
+	if (init <= end and flip):
+		$".".position.x += velocidade
+		$AnimatedSprite.flip_h = false
+		if($".".position.x >= end):
+			flip = false
+			
+	if ($".".position.x >= init and !flip):
+		$".".position.x -= velocidade
+		$AnimatedSprite.flip_h = true
+		if($".".position.x <= init):
+			flip = true
