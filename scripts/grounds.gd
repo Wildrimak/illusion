9227b4473d3da49c2686b8a855736a25bad3adce
Wildrimak <finalwildrimak@gmail.com>
2019-09-29 21:10:36 -0300

    Boneco Kinematico

--- scripts/grounds.gd
+++ scripts/grounds.gd
@@ -2,13 +2,20 @@ extends Position2D
 
 onready var soil = preload("res://scenes/soil.tscn")
 
+const IN_SCREEN = true
+
 func _ready():
 	pass
 		
+func _process(delta):
+	pass
+
 func _on_Timer_timeout():
 	print("Gerado")
 	var newSoil = soil.instance()
 	
 	newSoil.position.x = 0
 	newSoil.position.y = rand_range(-372,372)
-	add_child(newSoil)
+	get_owner().add_child(newSoil)
+		
+	
