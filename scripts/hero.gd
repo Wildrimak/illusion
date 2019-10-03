f9a62bf864230f87080dd0813ea59b95459ab1b5
Wildrimak <finalwildrimak@gmail.com>
2019-10-03 20:42:19 -0300

    Melhorando os movimentos

--- scripts/hero.gd
+++ scripts/hero.gd
@@ -6,26 +6,54 @@ const GRAVITY = 20
 const SPEED = 700
 const JUMP_HEIGHT = -850
 
+
+func movement_on_floor():
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.flip_h = false
+		
+		if Input.is_action_pressed("run"):
+			$AnimatedSprite.play("Run")
+			motion.x = SPEED * 2
+		else:
+			$AnimatedSprite.play("Walk")
+			motion.x = SPEED
+			
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.flip_h = true
+		
+		if Input.is_action_pressed("run"):
+			$AnimatedSprite.play("Run")
+			motion.x = -SPEED * 2
+		else:
+			$AnimatedSprite.play("Walk")
+			motion.x = -SPEED
+			
+	elif Input.is_action_pressed("ui_down"):
+		$AnimatedSprite.play("Dead")
+		if $AnimatedSprite.get_frame() == 14:
+			$AnimatedSprite.stop()
+	
+	else:
+		motion.x = 0
+		$AnimatedSprite.play("Idle")
+
+func movement_on_air():
+	movement_on_floor()
+
 func _physics_process(delta):
 	
 	motion.y += GRAVITY
 	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-		$AnimatedSprite.play("Walk")
-		$AnimatedSprite.flip_h = false
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-		$AnimatedSprite.flip_h = true
-	elif Input.is_action_pressed("ui_down"):
-		pass
-	else:
-		motion.x = 0
-		$AnimatedSprite.play("Idle")
-	
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
+		
+		movement_on_floor()	
+			
+		if Input.is_action_pressed("jump"):
+			$AnimatedSprite.play("Jump")
 			motion.y = JUMP_HEIGHT
+	
+	else:
+		movement_on_air()
 	
 	motion = move_and_slide(motion, UP)	
 	
